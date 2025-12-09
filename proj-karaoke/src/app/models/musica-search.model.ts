4bbaf8f227abdd109f32d480f1d3e07b84d5105b
LaisLopes <105338351+lahlops21@users.noreply.github.com>
2025-12-09 14:18:04 -0300

    Delete proj-karaoke directory

--- proj-karaoke/src/app/models/musica-search.model.ts
+++
@@ -1,5 +0,0 @@
-export interface MusicaSearch {
-  id_musica: number;
-  titulo: string;
-  codigo_musica: string;
-}
--- proj-karaoke/src/enviroments/enviroments.ts
+++
@@ -1,4 +0,0 @@
-
-export const enviroment = {
-    apiKaraoke: 'http://localhost:3000'
-}
